3e845550d4d7797bd08da71482a8c67eda946cb1
Myles Enriquez <enri.myles@gmail.com>
2019-05-02 11:05:35 -0700

    Add styling for different screen sizes.

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -7,21 +7,37 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin: 25px 0;
+`;
+
+const Move = styled.div`
+  padding: 10px;
+  margin-bottom: 50px;
+  border-radius: 3px;
+  @media only screen and (max-width: 450px) {
+    width: 75%;
+    border: 1px solid #7b2ce1;
+  }
 `;
 
 const Highlight = styled.b`
   color: #18e56e;
 `;
 
-const Move = styled.p`
+const P = styled.p`
+  margin: 0;
+  text-align: center;
   font-size: 24px;
   color: #e518dd;
+  @media only screen and (max-width: 768px) {
+    font-size: 36px;
+  }
 `;
 const Button = styled.button`
+  margin-bottom: 25px;
+  padding: 20px;
   font-family: "Fugaz One", cursive;
   font-size: 18px;
-  margin-bottom: 25px;
-  padding: 10px;
   border: none;
   border-radius: 3px;
   background-color: #7b2ce1;
@@ -35,6 +51,12 @@ const Button = styled.button`
   }
   &:active {
     transform: scale(0.75);
+  }
+  @media only screen and (max-width: 450px) {
+    width: 75%;
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 36px;
   }
 `;
 
@@ -73,11 +95,12 @@ class Generator extends React.Component {
   render() {
     return (
       <Container>
-        <Button onClick={this.handleClick}>Challenge Me</Button>
         <Move>
-          <Highlight>{this.state.category}</Highlight> from
-          <Highlight> {this.state.position}</Highlight>
+          <P><Highlight>{this.state.category}</Highlight></P> 
+          <P>from</P>
+          <P><Highlight> {this.state.position}</Highlight></P>
         </Move>
+        <Button onClick={this.handleClick}>Challenge Me</Button>
       </Container>
     );
   }
